16ec28ca87da363d6e7d4eca220f1bc99adc772c
KonkLaw <22473930+KonkLaw@users.noreply.github.com>
2022-12-22 21:26:36 +0100

    fixed: scrolling

--- AccountHelperWpf/Views/FileSortingView.xaml.cs
+++ AccountHelperWpf/Views/FileSortingView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace AccountHelperWpf.Views;
 /// <summary>
@@ -9,5 +10,12 @@ public partial class FileSortingView : UserControl
     public FileSortingView()
     {
         InitializeComponent();
+    }
+
+    private void UIElement_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        ScrollViewer scv = (ScrollViewer)sender;
+        scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+        e.Handled = true;
     }
 }
