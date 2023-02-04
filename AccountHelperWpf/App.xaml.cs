a385f59fd5d6d628a163ad8ea5c27ab0162baa75
KonkLaw <22473930+KonkLaw@users.noreply.github.com>
2023-02-04 15:32:12 +0100

    added: simple exception handling

--- AccountHelperWpf/App.xaml.cs
+++ AccountHelperWpf/App.xaml.cs
@@ -1,7 +1,21 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace AccountHelperWpf;
+
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application { }
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        Current.DispatcherUnhandledException += CurrentOnDispatcherUnhandledException;
+    }
+
+    private void CurrentOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(e.Exception.ToString());
+    }
+}
