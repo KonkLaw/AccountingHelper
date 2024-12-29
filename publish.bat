af00cf50df0e832d912fb7b844bf162630d66940
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2024-12-29 19:09:02 +0100

    improve publish sript
    specific naming will be used for updater

---
+++ publish.bat
@@ -0,0 +1,24 @@
+rmdir "Build" /S /Q
+mkdir "Build"
+dotnet publish "AccountHelperWpf\AccountHelperWpf.csproj" -c Release --framework net8.0-windows --self-contained true
+cd Build
+
+:: take first folder in the current directory
+for /d %%f in (*) do (
+    set "folder=%%f"
+    goto :found
+)
+
+:found
+
+cd "%folder%"
+:: remove all files in the folder
+for /d %%d in (*) do (
+    rmdir /s /q "%%d"
+)
+
+cd ..
+powershell Compress-Archive -Path "%folder%\*" -DestinationPath "%folder%.zip"
+
+rmdir "%folder%" /S /Q
+rename "%folder%.zip" "AccountHelper v%folder%.zip"
