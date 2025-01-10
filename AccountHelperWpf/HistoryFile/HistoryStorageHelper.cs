8db2dfad55d4867ef8282bce3c5ac6ff38f6d1f3
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2025-01-10 11:50:46 +0100

    add version for history file
    - version saving and checking

--- AccountHelperWpf/HistoryFile/HistoryStorageHelper.cs
+++ AccountHelperWpf/HistoryFile/HistoryStorageHelper.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO;
 using System.IO.Compression;
+using System.Numerics;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
+using System.Xml;
 
 namespace AccountHelperWpf.HistoryFile;
 
@@ -12,12 +14,15 @@ static class HistoryStorageHelper
 
     private const string HistoryFileName = "history.json";
 
+    private static readonly Version CurrentVersion = new Version(1, 0);
+
     private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
     {
         Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
         WriteIndented = true
     };
 
+    
     public static bool TryLoadFromFile(string path, out HistoryData? data, out string error)
     {
         using (var stream = new FileStream(path, FileMode.Open))
@@ -34,7 +39,34 @@ static class HistoryStorageHelper
 
                 using (Stream entryStream = entry.Open())
                 {
-                    data = JsonSerializer.Deserialize<HistoryData>(entryStream, Options);
+                    using (JsonDocument document = JsonDocument.Parse(entryStream))
+                    {
+                        data = default;
+                        if (!document.RootElement.TryGetProperty(nameof(HistoryFile.Version), out JsonElement versionElement))
+                        {
+                            error = "Too old version of history file";
+                            return false;
+                        }
+                        
+                        string? versionString = versionElement.GetString();
+                        if (versionString == null || !Version.TryParse(versionString, out Version? version))
+                        {
+                            error = "Version is not detected";
+                            return false;
+                        }
+
+                        if (version != CurrentVersion)
+                        {
+                            error = $"Version {version} is not supported";
+                            return false;
+                        }
+                    }
+                }
+
+                using (Stream entryStream = entry.Open())
+                {
+                    var fileData = JsonSerializer.Deserialize<HistoryFile>(entryStream, Options);
+                    data = fileData!.History;
                     error = string.Empty;
                     return true;
                 }
@@ -44,6 +76,12 @@ static class HistoryStorageHelper
 
     public static void Save(HistoryData associationData, string path)
     {
+        var fileData = new HistoryFile
+        {
+            Version = CurrentVersion,
+            History = associationData
+        };
+
         using (var archiveStream = new FileStream(path, FileMode.Create))
         {
             using (ZipArchive archive = new(archiveStream, ZipArchiveMode.Create))
@@ -51,9 +89,16 @@ static class HistoryStorageHelper
                 ZipArchiveEntry entry = archive.CreateEntry(HistoryFileName);
                 using (Stream entryStream = entry.Open())
                 {
-                    JsonSerializer.Serialize(entryStream, associationData, Options);
+                    JsonSerializer.Serialize(entryStream, fileData, Options);
                 }
             }
         }
+    }
+
+
+    class HistoryFile
+    {
+        public Version? Version { get; set; }
+        public HistoryData? History { get; set; }
     }
 }
